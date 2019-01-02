8b5f73e1a0baa230b03c8821c6e3e4b2c15e6c4f
lviit <lauriviitala3000@gmail.com>
2019-01-02 23:11:43 +0200

    constants.js

---
+++ src/constants.js
@@ -0,0 +1,5 @@
+export const API_URI = "https://api.github.com/graphql";
+export const API_PREVIEW_ACCEPT_HEADER =
+  "application/vnd.github.hawkgirl-preview+json";
+export const GITHUB_README_URL =
+  "https://help.github.com/articles/listing-the-packages-that-a-repository-depends-on/#enabling-the-dependency-graph-for-a-private-repository";
